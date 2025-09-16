1863e11e7f877b5df2d15f0a3e90c503f52700db
Miguel Moreno <mamorefp@gmail.com>
2025-09-16 08:16:31 +0000

    Modificado código para el gráfico 2

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -8,16 +8,15 @@ register_matplotlib_converters()
 df = pd.read_csv('fcc-forum-pageviews.csv', index_col='date', parse_dates=True)
 
 # Clean data
-df = df[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
+df = None
 
 
 def draw_line_plot():
     # Draw line plot
-    plt.figure(figsize=(16,6))
-    plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
-    plt.xlabel('Date')
-    plt.ylabel('Page Views')
-    plt.plot(df.value, 'r');   
+
+
+
+
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
@@ -25,18 +24,14 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = df.groupby(['year', 'month'])['value'].mean().reset_index()
+    df_bar = None
 
     # Draw bar plot
-    import calendar
 
-    plt.figure(figsize=(8,6.6))
-    plt.xticks(rotation=90)
-    plt.xlabel('Years')
-    plt.ylabel('Average Page Views')
-    ax = sns.barplot(data=df_bar, x='year', y='value', hue='month', hue_order=list(calendar.month_name)[1:], palette='tab10', width=0.5)
-    ax.legend(title='Months');
-    
+
+
+
+
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
     return fig
